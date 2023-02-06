472922a9cac92329a4b815d504bff1a46d8ed82b
ytellon <ytellonmacedo@gmail.com>
2023-02-06 15:49:57 -0300

    feat: implementado a func remove capaz de remover o primeiro arquivo processado

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,9 +1,24 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+import sys
 
+def process(path_file, instance: Queue):
+    file_lines = txt_importer(path_file)
+    dict_file = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_lines),
+        "linhas_do_arquivo": file_lines
+    }
+    if dict_file not in instance.list:
+        instance.enqueue(dict_file)
+        sys.stdout.write(f"{dict_file}")
 
-def remove(instance):
-    """Aqui irá sua implementação"""
+def remove(instance: Queue):
+    if len(instance) == 0:
+        sys.stdout.write("Não há elementos\n")
+    else:
+        saved_dequeue = instance.dequeue()["nome_do_arquivo"]
+        sys.stdout.write(f"Arquivo {saved_dequeue} removido com sucesso\n")
 
 
 def file_metadata(instance, position):
