a3bfd2dac63f75254afc978028f49bd69204e8a9
ytellon <ytellonmacedo@gmail.com>
2023-02-06 16:23:03 -0300

    feat: implementado a func exist_world que verifica a existencia de uma palavra em arquivos

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+
+
+def exists_word(word, instance: Queue):
+    def_list = list()
+    queue_list = instance.list
+    for files in queue_list:
+        ocurrencies_list = list()
+        for index, line in enumerate(files["linhas_do_arquivo"], start=1):
+            if word.lower() in line.lower():
+                ocurrencies_list.append({"linha": index})
+        if len(ocurrencies_list) > 0:
+            def_list.append({
+                "palavra": word,
+                "arquivo": files["nome_do_arquivo"],
+                "ocorrencias": ocurrencies_list
+            })
+    return def_list
 
 
 def search_by_word(word, instance):
