b6b10a6a896c97952f874720f82cc768e0935fbc
ytellon <ytellonmacedo@gmail.com>
2023-02-08 19:02:33 -0300

    fix: update lint errors

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -2,6 +2,7 @@ from ting_file_management.queue import Queue
 from ting_file_management.file_management import txt_importer
 import sys
 
+
 def process(path_file, instance: Queue):
     file_lines = txt_importer(path_file)
     dict_file = {
@@ -12,6 +13,7 @@ def process(path_file, instance: Queue):
     if dict_file not in instance.list:
         instance.enqueue(dict_file)
         sys.stdout.write(f"{dict_file}")
+
 
 def remove(instance: Queue):
     if len(instance) == 0:
